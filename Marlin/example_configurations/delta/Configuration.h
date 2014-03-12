24e0b43ca395a0da50a1c6c10346f8e8ca982ab5
Su <Gfast2@hotmail.com>
2014-03-12 18:39:41 +0100

    Selecte a correct power supply

--- Marlin/example_configurations/delta/Configuration.h
+++ Marlin/example_configurations/delta/Configuration.h
@@ -86,7 +86,7 @@
 // 1 = ATX
 // 2 = X-Box 360 203Watts (the blue wire connected to PS_ON and the red wire to VCC)
 
-#define POWER_SUPPLY 1
+#define POWER_SUPPLY 2
 
 // Define this to have the electronics keep the powersupply off on startup. If you don't know what this is leave it.
 // #define PS_DEFAULT_OFF
